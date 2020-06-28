525a5d057445187676e81a19fb09e95548e9f97a
Saad <khan.ssaad@gmail.com>
2020-06-28 17:27:29 +0530

    Save article from browser and validation messages display on browser

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,19 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new
+    end
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+          #redirect_to article_path(@article)
+          flash[:notice] = "Article was saved successfully"
+          redirect_to @article    #this also works same as above
+        else 
+            render 'new'
+        end      
+    end
 end
